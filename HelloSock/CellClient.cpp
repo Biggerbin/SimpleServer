19dffb2a8ced9f69eeed023af5c5cd32679ee297
BiggerBin <13210043829@163.com>
2021-07-09 14:59:36 +0800

    服务端加入了定时定量发送的能力

--- HelloSock/CellClient.cpp
+++ HelloSock/CellClient.cpp
@@ -13,6 +13,19 @@ CellClient::~CellClient()
 	closesocket(_cli_sock);
 }
 
+int CellClient::SendReal()
+{
+	if (_lastSendPos > 0) {
+		int ret = send(_cli_sock, _szSendBuf, _lastSendPos, 0);
+		if (ret == SOCKET_ERROR) {
+			return -1;
+		}
+		_lastSendPos = 0;
+		resetDTSend();
+	}
+	return 0;
+}
+
 
 int CellClient::SendData(dataHeaderPtr header)
 {
@@ -53,4 +66,21 @@ bool CellClient::checkHeart(time_t dt)
 void CellClient::resetDTHeart()
 {
 	_dtHeart = 0;
+}
+
+bool CellClient::checkSend(time_t dt)
+{
+	_dtSend += dt;
+	if (_dtSend >= CLIENT_SEND_MSG_TIEM) {
+		printf("checkSend dead:s=%d,time=%d\n", _cli_sock, _dtSend);
+		SendReal();
+		resetDTSend();
+		return true;
+	}
+	return false;
+}
+
+void CellClient::resetDTSend()
+{
+	_dtSend = 0;
 }
--- HelloSock/CellClient.h
+++ HelloSock/CellClient.h
@@ -1,15 +1,20 @@
 #ifndef _CELLCLIENT_H_
 #define _CELLCLIENT_H_
 #define CLIENT_HREAT_DEAD_TIME 5000
+#define CLIENT_SEND_MSG_TIEM 200
+
 #include "CellCommon.h"
 typedef std::shared_ptr<DataHeader> dataHeaderPtr;
 struct CellClient : public ObjectPoolBase<CellClient, 1000> {
 public:
 	CellClient(SOCKET cliSock);
 	~CellClient();
+	int SendReal();
 	int SendData(dataHeaderPtr header);
 	bool checkHeart(time_t dt);
 	void resetDTHeart();
+	bool checkSend(time_t dt);
+	void resetDTSend();
 	int _cli_sock;
 	//接收缓冲区
 	char _recvBuf1[RECV_BUF_SIZE];
@@ -22,6 +27,7 @@ public:
 	//发送缓冲区的数据尾部位置
 	int _lastSendPos = 0;
 	time_t _dtHeart;
+	time_t _dtSend;
 
 };
 
